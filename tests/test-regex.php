2f1e2e0b99e85ad74cdedbc3a847834e88385376
Daniel Bachhuber <daniel@handbuilt.co>
2019-11-26 11:04:27 -0800

    Configure test suite

---
+++ tests/test-regex.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Tests regex patterns.
+ *
+ * @package Broken_Link_Fixer
+ */
+
+/**
+ * Tests regex patterns.
+ */
+class Test_Regex extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
